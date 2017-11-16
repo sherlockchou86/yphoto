2e8855329dd469c1d1e6a265a14ea21185b455ad
sherlockchou86 <zhouzhi9@foxmail.com>
2017-11-16 14:57:54 +0800

    add location search UI

--- YPhoto/app/src/main/java/com/yphoto/zhzhi/yphoto/LocationSearchActivity.java
+++ YPhoto/app/src/main/java/com/yphoto/zhzhi/yphoto/LocationSearchActivity.java
@@ -1,10 +1,8 @@
 package com.yphoto.zhzhi.yphoto;
 
-import android.content.Context;
-import android.util.AttributeSet;
-import android.view.LayoutInflater;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.widget.ListView;
-import android.widget.RelativeLayout;
 import android.widget.SimpleAdapter;
 
 import com.amap.api.maps.model.LatLng;
@@ -14,42 +12,26 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-/**
- * Created by zhzhi on 10/21/2017.
- */
+public class LocationSearchActivity extends AppCompatActivity {
 
-public class MenuView extends RelativeLayout {
-    public MenuView(Context context) {
-        super(context);
-        initView(context);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
+        setContentView(R.layout.activity_location_search);
+
+        init();
     }
 
-    public MenuView(Context context, AttributeSet attrs) {
-        super(context, attrs);
-        initView(context);
-    }
+    private void init() {
+        ListView search_list = (ListView) findViewById(R.id.search_list);
 
-    public MenuView(Context context, AttributeSet attrs, int defStyleAttr) {
-        super(context, attrs, defStyleAttr);
-        initView(context);
-    }
-
-    private void initView(Context context) {
-        LayoutInflater.from(context).inflate(R.layout.view_menu, this, true);
-
-        // 加载收藏位置
-        loadLocations();
-    }
-
-    private void loadLocations() {
-        ListView location_list = (ListView) findViewById(R.id.location_favorites_list);
-
-        SimpleAdapter adapter = new SimpleAdapter(getContext(),
+        SimpleAdapter adapter = new SimpleAdapter(this,
                 getLocationList(),
                 R.layout.item_location_favorites,
                 new String[]{"name", "detail", "item_type"},
                 new int[]{R.id.location_name, R.id.location_detail, R.id.item_type_icon});
-        location_list.setAdapter(adapter);
+        search_list.setAdapter(adapter);
     }
 
     private List<Map<String, Object>> getLocationList() {
@@ -60,91 +42,91 @@ public class MenuView extends RelativeLayout {
         map.put("name", "中国电子大厦");
         map.put("detail", "海淀区-丹棱街5号");
         map.put("loc", new LatLng(1,1));  // 该位置的具体经纬度\
-        map.put("item_type", R.drawable.star);
+        map.put("item_type", R.drawable.up_left);
         list.add(map);
 
         map = new HashMap<String, Object>();
         map.put("name", "北京东方君悦大酒店");
         map.put("detail", "北京市-东城区-东长安街1号东方广场");
         map.put("loc", new LatLng(1,1));  // 该位置的具体经纬度
-        map.put("item_type", R.drawable.star);
+        map.put("item_type", R.drawable.up_left);
         list.add(map);
 
         map = new HashMap<String, Object>();
         map.put("name", "百老汇影城");
         map.put("detail", "海淀区-丹棱街5号");
         map.put("loc", new LatLng(1,1));  // 该位置的具体经纬度
-        map.put("item_type", R.drawable.star);
+        map.put("item_type", R.drawable.up_left);
         list.add(map);
 
         map = new HashMap<String, Object>();
         map.put("name", "奥林匹克公园");
         map.put("detail", "北京市-朝阳区-科荟路32号");
         map.put("loc", new LatLng(1,1));  // 该位置的具体经纬度
-        map.put("item_type", R.drawable.star);
+        map.put("item_type", R.drawable.up_left);
         list.add(map);
 
         map = new HashMap<String, Object>();
         map.put("name", "中国电子大厦");
         map.put("detail", "海淀区-丹棱街5号");
         map.put("loc", new LatLng(1,1));  // 该位置的具体经纬度
-        map.put("item_type", R.drawable.star);
+        map.put("item_type", R.drawable.up_left);
         list.add(map);
 
         map = new HashMap<String, Object>();
         map.put("name", "中国电子大厦");
         map.put("detail", "海淀区-丹棱街5号");
         map.put("loc", new LatLng(1,1));  // 该位置的具体经纬度
-        map.put("item_type", R.drawable.star);
+        map.put("item_type", R.drawable.up_left);
         list.add(map);
 
         map = new HashMap<String, Object>();
         map.put("name", "中国电子大厦");
         map.put("detail", "海淀区-丹棱街5号");
         map.put("loc", new LatLng(1,1));  // 该位置的具体经纬度
-        map.put("item_type", R.drawable.star);
+        map.put("item_type", R.drawable.up_left);
         list.add(map);
 
         map = new HashMap<String, Object>();
         map.put("name", "中国电子大厦");
         map.put("detail", "海淀区-丹棱街5号");
         map.put("loc", new LatLng(1,1));  // 该位置的具体经纬度
-        map.put("item_type", R.drawable.star);
+        map.put("item_type", R.drawable.up_left);
         list.add(map);
 
         map = new HashMap<String, Object>();
         map.put("name", "中国电子大厦");
         map.put("detail", "海淀区-丹棱街5号");
         map.put("loc", new LatLng(1,1));  // 该位置的具体经纬度
-        map.put("item_type", R.drawable.star);
+        map.put("item_type", R.drawable.up_left);
         list.add(map);
 
         map = new HashMap<String, Object>();
         map.put("name", "中国电子大厦");
         map.put("detail", "海淀区-丹棱街5号");
         map.put("loc", new LatLng(1,1));  // 该位置的具体经纬度
-        map.put("item_type", R.drawable.star);
+        map.put("item_type", R.drawable.up_left);
         list.add(map);
 
         map = new HashMap<String, Object>();
         map.put("name", "中国电子大厦");
         map.put("detail", "海淀区-丹棱街5号");
         map.put("loc", new LatLng(1,1));  // 该位置的具体经纬度
-        map.put("item_type", R.drawable.star);
+        map.put("item_type", R.drawable.up_left);
         list.add(map);
 
         map = new HashMap<String, Object>();
         map.put("name", "中国电子大厦");
         map.put("detail", "海淀区-丹棱街5号");
         map.put("loc", new LatLng(1,1));  // 该位置的具体经纬度
-        map.put("item_type", R.drawable.star);
+        map.put("item_type", R.drawable.up_left);
         list.add(map);
 
         map = new HashMap<String, Object>();
         map.put("name", "中国电子大厦");
         map.put("detail", "海淀区-丹棱街5号");
         map.put("loc", new LatLng(1,1));  // 该位置的具体经纬度
-        map.put("item_type", R.drawable.star);
+        map.put("item_type", R.drawable.up_left);
         list.add(map);
 
         return list;
--- YPhoto/app/src/main/java/com/yphoto/zhzhi/yphoto/SearchView.java
+++ YPhoto/app/src/main/java/com/yphoto/zhzhi/yphoto/SearchView.java
@@ -1,6 +1,7 @@
 package com.yphoto.zhzhi.yphoto;
 
 import android.content.Context;
+import android.content.Intent;
 import android.util.AttributeSet;
 import android.view.Gravity;
 import android.view.LayoutInflater;
@@ -33,13 +34,24 @@ public class SearchView extends RelativeLayout implements View.OnClickListener {
 
         final View view_avatar = findViewById(R.id.avatar);
         view_avatar.setOnClickListener(this);   //点击头像
+
+        final View search_box = findViewById(R.id.search_box);
+        search_box.setOnClickListener(this);
+
+        final View search_btn = findViewById(R.id.search_button);
+        search_btn.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v){
-        MenuView menu = new MenuView(getContext());
-        PopupWindow popup = new PopupWindow(menu, LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT, true);
-        popup.setAnimationStyle(R.style.MenuAnimation);
-        popup.showAtLocation(this, Gravity.NO_GRAVITY, 0, 0);
+        if (v.getId() == R.id.avatar) {
+            MenuView menu = new MenuView(getContext());
+            PopupWindow popup = new PopupWindow(menu, LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT, true);
+            popup.setAnimationStyle(R.style.MenuAnimation);
+            popup.showAtLocation(this, Gravity.NO_GRAVITY, 0, 0);
+        } else if (v.getId() == R.id.search_box || v.getId() == R.id.search_button) {
+            Intent intent = new Intent(getContext(), LocationSearchActivity.class);
+            getContext().startActivity(intent);
+        }
     }
 }
